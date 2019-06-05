bab6395f85f1dd55d2e37a3302f3c1d8ce0b726d
chee <chee@snoot.club>
2019-06-05 14:42:44 +0100

    Allow taking stream from stdin

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 let {promisify} = require("util")
-let {existsSync, promises: fs} = require("fs")
+let {existsSync, createReadStream, promises: fs} = require("fs")
 let resolvePath = require("path").resolve
 let findColor = require("./find-color").find
 let readDataUri = require("data-uri-to-buffer")
-let createSlices = promisify(require("image-to-slices"))
 let {EOL} = require("os")
 let getImageSize = require("image-size")
 let Canvas = require("canvas")
 let Colorthief = require("color-thief")
 let colors = require("blee").all
+
+let symbols = {
+	readFromStdin: Symbol("read from stdin"),
+}
 
 function getColorFromName(name) {
 	let color = colors.get(name)
@@ -21,7 +24,7 @@ function getColorFromName(name) {
 }
 
 let {argv} = require("yargs").command({
-	command: "$0 <image>",
+	command: "$0 [image]",
 	describe: "make an emoji mosaic from an image",
 	builder: yargs => {
 		yargs
@@ -43,14 +46,17 @@ let {argv} = require("yargs").command({
 			})
 			.positional("image", {
 				describe: "the image to turn into an emoji mosaic",
-				validate: existsSync,
+				default: symbols.readFromStdin,
+				coerce: path => {
+					if (path === symbols.readFromStdin || Object.is(path, true)) {
+						return symbols.readFromStdin
+					}
+					if (!existsSync(path)) {
+						throw new Error(`expected a file that exists, got: ${path}`)
+					}
+					return path
+				},
 			})
-	},
-})
-
-createSlices.configure({
-	clipperOptions: {
-		canvas: require("canvas"),
 	},
 })
 
@@ -92,14 +98,18 @@ function createLines(n) {
 	return createRange(argv.tilesize, n, argv.tilesize)
 }
 
-async function getImageContext(imageData, size = getImageSize(imageData)) {
+async function getImage(imageData, size = getImageSize(imageData)) {
 	let canvas = Canvas.createCanvas(size.width, size.height)
 	let context = canvas.getContext("2d")
 	let imageElement = new Canvas.Image()
 	return new Promise((resolve, reject) => {
 		imageElement.onload = () => {
 			context.drawImage(imageElement, 0, 0)
-			resolve(context)
+			resolve({
+				imageElement,
+				context,
+				size,
+			})
 		}
 		imageElement.onerror = reject
 		imageElement.src = imageData
@@ -112,18 +122,66 @@ function getDominantColor(buffer) {
 	return colorthief.getColor(buffer)
 }
 
-;(async function beepBoopBork() {
+// read a stream into memory
+async function unstream(stream) {
+	let chunks = []
+	let push = chunk => chunks.push(chunk)
+	stream.on("data", chunk => chunks.push(chunk))
+	return new Promise((keep, brake) => {
+		stream.on("end", () => {
+			let buffer = Buffer.concat(chunks)
+			keep(buffer)
+		})
+		stream.on("error", brake)
+	})
+}
+
+function sliceImage({context, size: {width, height}, tilesize}) {
+	let columns = width / tilesize
+	let rows = height / tilesize
+	let slices = []
+	for (let y = 0; y < rows; ++y) {
+		for (let x = 0; x < columns; ++x) {
+			let sliceCanvas = Canvas.createCanvas(tilesize, tilesize)
+			let sliceContext = sliceCanvas.getContext("2d")
+			let point = [x * tilesize, y * tilesize]
+			sliceContext.drawImage(
+				context.canvas,
+				...point,
+				tilesize,
+				tilesize,
+				0,
+				0,
+				width,
+				height
+			)
+			slices.push({
+				x: point[0],
+				y: point[1],
+				url: sliceCanvas.toDataURL(),
+			})
+		}
+	}
+	return slices
+}
+
+(async function beepBoopBork() {
 	let database = await readDatabase(databasePath)
 	let emojiColors = database.map(line => line.color)
-	let imageData = await fs.readFile(argv.image)
-	let context = await getImageContext(imageData)
-	let {width, height} = getImageSize(imageData)
+	let imageStream =
+		argv.image === symbols.readFromStdin
+			? process.stdin
+			: createReadStream(argv.image)
+	let imageData = await unstream(imageStream)
+	let {context, size} = await getImage(imageData)
+	let {width, height} = size
 	let lines = [createLines(height), createLines(width)]
 
-	// this always throws? ^_^ what
-	let slices = await createSlices(argv.image, ...lines, {
-		saveToDataUrl: true,
-	}).catch(what => what)
+	let slices = sliceImage({
+		context,
+		size,
+		tilesize: argv.tilesize,
+	})
 
 	// all in one big loop !!
 	let previousSlice
@@ -131,7 +189,7 @@ function getDominantColor(buffer) {
 		if (previousSlice && slice.y > previousSlice.y) {
 			print(EOL)
 		}
-		let buffer = readDataUri(slice.dataURI)
+		let buffer = readDataUri(slice.url)
 		let dominantColor = await getDominantColor(buffer)
 		let color = dominantColor || argv.background
 		let closest = findColor(color, emojiColors)
